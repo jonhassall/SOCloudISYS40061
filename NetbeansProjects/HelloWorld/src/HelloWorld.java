cec0022859b7be5d8cdf246fc524c806a6f53f08
jonhassall <jon@jonhassall.com>
2015-01-13 21:57:57 +0000

    Hello world

---
+++ NetbeansProjects/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+    
+}
