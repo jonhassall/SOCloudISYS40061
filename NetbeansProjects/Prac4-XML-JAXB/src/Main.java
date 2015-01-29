0ad03eca4f82b83504559eff1e397c7db3eda835
Jonathan Hassall <jon@jonhassall.com>
2015-01-29 13:13:52 +0000

    Add Lab 4 XML starting point

---
+++ NetbeansProjects/Prac4-XML-JAXB/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author taha-m
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
